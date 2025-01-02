a4310c438a213cbe9f2eaa8e90f1aec385767cd9
또우 <ddowoot@inflab.com>
2025-01-03 08:18:55 +0900

    Skia Shader 학습중

--- App.tsx
+++ App.tsx
@@ -1,10 +1,11 @@
-import VictoryChart from './src/screens/victoryChart';
 import {GestureHandlerRootView} from 'react-native-gesture-handler';
+import SkiaShader from './src/screens/SkiaShader';
 
 const App = () => {
   return (
     <GestureHandlerRootView>
-      <VictoryChart />
+
+      <SkiaShader />
     </GestureHandlerRootView>
   );
 };

3098974a90970d2bd8f7e68ca56e25b0bd7f7110
또우 <ddowoot@inflab.com>
2025-01-09 01:01:41 +0900

    격자 모양에 따른 블럭 옴기기 기능 추가

---
+++ src/screens/gestureGrid/constants/mountain.ts
@@ -0,0 +1,7 @@
+export const MOUNTAIN_GRID = [
+  [false, false, false, true, false, false, false],
+  [false, false, true, true, true, false, false],
+  [false, false, false, true, false, false, false],
+  [false, true, true, true, true, true, false],
+  [true, true, true, true, true, true, true],
+];
